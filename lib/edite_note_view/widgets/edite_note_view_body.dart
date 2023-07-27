0520a4a3ada7a4d064c8bac648aec1110a9fcb32
ka1395 <ka1395@fayoum.ed.eg>
2023-07-27 16:10:02 +0300

    edite note view ui done

--- lib/edite_note_view/widgets/edite_note_view_body.dart
+++ lib/edite_note_view/widgets/edite_note_view_body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/help_widgets/custom_appbar.dart';
 
+import '../../help_widgets/custom_text_field.dart';
+
 class EditeNoteViewBody extends StatelessWidget {
   const EditeNoteViewBody({super.key});
 
@@ -13,7 +15,15 @@ class EditeNoteViewBody extends StatelessWidget {
           SizedBox(
             height: 50,
           ),
-          CustomAppBar(title: "Edite Note", iconData: Icons.done)
+          CustomAppBar(title: "Edite Note", iconData: Icons.done),
+          SizedBox(
+            height: 50,
+          ),
+          CustomTextField(hint: "Title"),
+          SizedBox(
+            height: 16,
+          ),
+          CustomTextField(hint: "Content", maxLine: 5),
         ],
       ),
     );
